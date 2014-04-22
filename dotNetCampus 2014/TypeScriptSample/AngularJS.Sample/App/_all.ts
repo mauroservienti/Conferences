e57222ab8c0831e97af8802d877712caaeee3406
Mauro Servienti <mauro@topics.it>
2014-04-22 08:54:42 +0200

    initial setup

--- dotNetCampus 2014/TypeScriptSample/AngularJS.Sample/App/_all.ts
+++
@@ -1,4 +0,0 @@
-﻿/// <reference path='../scripts/typings/jquery/jquery.d.ts' />
-/// <reference path='../scripts/typings/angularjs/angular.d.ts' />
-/// <reference path='Interfaces/IAboutScope.ts' />
-/// <reference path='Models/person.ts' />
--- dotNetCampus 2014/TypeScriptSample/AngularJS.Sample/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace AngularJS.Sample.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
