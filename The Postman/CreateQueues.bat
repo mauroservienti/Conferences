719730616b6162db8a7c689ced9e0ce762390a35
Mauro Servienti <mauro@topics.it>
2014-08-09 15:32:13 +0200

    qc

---
+++ The Postman/CreateQueues.bat
@@ -0,0 +1,5 @@
+start %~dp0\tools\QueueCreator\qc.exe -acwu -n=MailSender
+start %~dp0\tools\QueueCreator\qc.exe -acwu -n=MailRecipient
+start %~dp0\tools\QueueCreator\qc.exe -acwu -n=MailServer.Milan
+start %~dp0\tools\QueueCreator\qc.exe -acwu -n=MailServer.Dublin
+start %~dp0\tools\QueueCreator\qc.exe -acwu -n=MailServer.London
