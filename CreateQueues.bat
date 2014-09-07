79b631dd93407e4167be851c57168216c2013488
Mauro Servienti <mauro@topics.it>
2014-09-07 17:20:09 +0200

    uhm...

--- CreateQueues.bat
+++
@@ -1,5 +0,0 @@
-start %~dp0\tools\QueueCreator\qc.exe -acwu -n=MailSender
-start %~dp0\tools\QueueCreator\qc.exe -acwu -n=MailRecipient
-start %~dp0\tools\QueueCreator\qc.exe -acwu -n=MailServer.Milan
-start %~dp0\tools\QueueCreator\qc.exe -acwu -n=MailServer.Dublin
-start %~dp0\tools\QueueCreator\qc.exe -acwu -n=MailServer.London
