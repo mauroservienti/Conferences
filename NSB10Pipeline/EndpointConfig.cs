92ba8ad3732b0f5acddf305bdb36099de5127fef
Mauro Servienti <mauro@topics.it>
2014-10-08 09:56:17 +0200

    pipeline and features

---
+++ NSB10Pipeline/EndpointConfig.cs
@@ -0,0 +1,55 @@
+
+namespace NSB10Pipeline
+{
+	using NServiceBus;
+	using NServiceBus.Pipeline;
+	using NServiceBus.Pipeline.Contexts;
+
+    /*
+		This class configures this endpoint as a Server. More information about how to configure the NServiceBus host
+		can be found here: http://particular.net/articles/the-nservicebus-host
+	*/
+    public class EndpointConfig : IConfigureThisEndpoint, AsA_Server
+    {
+        public void Customize(BusConfiguration configuration)
+        {
+            configuration.UsePersistence<InMemoryPersistence>();
+			configuration.Pipeline.Register( "MyStep", typeof( MyBehavior ), "behavior description" );
+			configuration.Pipeline.Register<MyOtherBehaviorSetup>();
+        }
+    }
+
+	class MyBehavior : IBehavior<IncomingContext>
+	{
+		public void Invoke( IncomingContext context, System.Action next )
+		{
+			//can do something before moving on
+
+			next();
+
+			//can do something after the message has been handled
+		}
+	}
+
+	class MyOtherBehaviorSetup : RegisterStep 
+	{
+		public MyOtherBehaviorSetup()
+			: base( "MyOtherBehavior" , typeof(MyOtherBehavior), "description here..." )
+		{
+			InsertAfter( WellKnownStep.DeserializeMessages );
+		}
+	}
+
+	class MyOtherBehavior : IBehavior<IncomingContext>
+	{
+		public void Invoke( IncomingContext context, System.Action next )
+		{
+			//can do something before moving on
+
+			next();
+
+			//can do something after the message has been handled
+		}
+	}
+
+}
---
+++ NSB11Features/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NSB11Features")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NSB11Features")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5ba9a325-2161-4148-8a18-c56c37a383da")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
