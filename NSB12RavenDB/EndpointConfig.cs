1d86e6ae35602cdff2619c4f84877176b1faccd1
Mauro Servienti <mauro@topics.it>
2014-10-08 14:47:14 +0200

    persistence

---
+++ NSB12RavenDB/EndpointConfig.cs
@@ -0,0 +1,24 @@
+
+namespace NSB12RavenDB
+{
+    using NServiceBus;
+    using NServiceBus.Persistence;
+
+    /*
+		This class configures this endpoint as a Server. More information about how to configure the NServiceBus host
+		can be found here: http://particular.net/articles/the-nservicebus-host
+	*/
+    public class EndpointConfig : IConfigureThisEndpoint, AsA_Server
+    {
+        public void Customize(BusConfiguration configuration)
+        {
+            configuration.UsePersistence<RavenDBPersistence>()
+                .DoNotSetupDatabasePermissions()
+                .For( Storage.Sagas, Storage.Subscriptions );
+
+            configuration.UsePersistence<InMemoryPersistence>()
+                .For( Storage.Timeouts );
+
+        }
+    }
+}
