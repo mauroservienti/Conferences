b55d42afe68fb5bd5d71633ab91e94f1f3f52552
Mauro Servienti <mauro@topics.it>
2014-10-08 07:30:19 +0200

    hosting samples

---
+++ NSB06BuiltinHost/EndpointConfig.cs
@@ -0,0 +1,29 @@
+
+namespace NSB06BuiltinHost
+{
+    using NServiceBus;
+
+	/*
+		This class configures this endpoint as a Server. More information about how to configure the NServiceBus host
+		can be found here: http://particular.net/articles/the-nservicebus-host
+	*/
+	public class EndpointConfig : IConfigureThisEndpoint, AsA_Server
+    {
+
+    }
+
+    public class CustomInitialization : IWantCustomInitialization
+    {
+        public void Init()
+        {
+            var embeddedSore = new Raven.Client.Embedded.EmbeddableDocumentStore
+            {
+                DataDirectory = @"~\..\RavenDB\Data"
+            }.Initialize();
+
+            Configure.Instance.DefiningCommandsAs( t => t.Namespace != null && t.Namespace.EndsWith( ".Commands" ) );
+            Configure.Instance.DefiningEventsAs( t => t.Namespace != null && t.Namespace.EndsWith( ".Events" ) );
+            Configure.Instance.RavenPersistenceWithStore( embeddedSore );
+        }
+    }
+}
---
+++ NSB06SelfHost/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus;
+
+namespace NSB06SelfHost
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            var embeddedSore = new Raven.Client.Embedded.EmbeddableDocumentStore
+            {
+                DataDirectory = @"~\..\RavenDB\Data"
+            }.Initialize();
+
+
+            NServiceBus.Configure.With()
+                .DefaultBuilder()
+                .Log4Net()
+                .DefiningCommandsAs( t => t.Namespace != null && t.Namespace.EndsWith( ".Commands" ) )
+                .DefiningEventsAs( t => t.Namespace != null && t.Namespace.EndsWith( ".Events" ) )
+                .RavenPersistenceWithStore( embeddedSore )
+                .UnicastBus()
+                .LoadMessageHandlers()
+                .CreateBus()
+                .Start();
+
+            Console.Read();
+        }
+    }
+}
