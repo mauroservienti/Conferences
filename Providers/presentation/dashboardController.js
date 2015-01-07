c00ecfaf8c3f746f81fa29014483b7e811c197bc
Mauro Servienti <mauro@topics.it>
2015-01-07 14:48:07 +0100

    refactoring the sample

--- Providers/presentation/dashboardController.js
+++
@@ -1,9 +0,0 @@
-(function(){
-
-	angular.module('sample.controllers')
-		.controller('dashboardController', ['$log', 'sampleService', function($log, sampleService){
-			var vm = this;
-			vm.setting = sampleService.setting;
-	}]);
-
-}())
