c7e7133e2c788a6ec68437beb19a26db7c2bfe3d
Mauro Servienti <mauro@topics.it>
2015-10-20 23:15:26 +0200

    initial commit

--- appModule.js
+++ appModule.js
@@ -5,50 +5,19 @@
     angular.module('sample.services', []);
 
     var sampleApp = angular.module('sample.App', [
-                'ngRoute',
-                'ngAnimate',
-                'ui.router',
-                'ui.bootstrap',
-                'cgBusy',
                 'sample.controllers',
                 'sample.services'
     ]);
 
-    sampleApp.config(['$stateProvider', '$locationProvider', '$logProvider', 'sampleServiceProvider',
-            function ( $stateProvider, $locationProvider, $logProvider, sampleServiceProvider) {
+    sampleApp.config(['$locationProvider', '$logProvider', 'sampleServiceProvider',
+            function ( $locationProvider, $logProvider, sampleServiceProvider) {
 
                 $logProvider.debugEnabled(true);
-                sampleServiceProvider.aSetting = 'this is my setting';
-                
-                var rootViews = {
-                    '': {
-                        templateUrl: '/presentation/dashboardView.html',
-                        controller: 'dashboardController as dashboard'
-                    }
-                };
-
-                $stateProvider
-                    .state('root', {
-                        url: '',
-                        views: rootViews
-                    })
-                    .state('dashboard', {
-                        url: '/',
-                        views: rootViews
-                    });
-
                 $locationProvider.html5Mode(false);
+                
+                sampleServiceProvider.aSetting = 'this is my setting';
 
                 console.debug('configuration completed.');
             }]);
-
-    sampleApp.run(['$log', '$state', '$rootScope', '$stateParams',
-        function ( $log, $state, $rootScope, $stateParams) {
-
-            $rootScope.$state = $state;
-            $rootScope.$log = $log;
-            $rootScope.$stateParams = $stateParams;
-
-        }]);
 
 }());
--- sampleService.js
+++ sampleService.js
@@ -1,3 +1,4 @@
+/* global angular */
 (function(){
 
 	angular.module('sample.services')
