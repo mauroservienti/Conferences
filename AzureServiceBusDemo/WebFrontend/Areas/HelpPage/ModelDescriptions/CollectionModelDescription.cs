feb11e7ffb3478e697c7a92833f4d6ac8aaa090c
Mauro Servienti <mauro@topics.it>
2015-11-13 11:34:58 +0100

    initial commit

---
+++ AzureServiceBusDemo/WebFrontend/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace WebFrontend.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ AzureServiceBusDemo/WebFrontend/WebRole.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Diagnostics;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using SharedMessages;
+
+namespace WebFrontend
+{
+    public class WebRole : RoleEntryPoint
+    {
+        
+    }
+}
