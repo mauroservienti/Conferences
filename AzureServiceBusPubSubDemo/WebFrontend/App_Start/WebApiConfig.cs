2b8125a7bcc2957979753a2184029ac55da18734
Mauro Servienti <mauro@topics.it>
2015-11-24 15:41:47 +0100

    Initial commit

---
+++ AzureServiceBusPubSubDemo/WebFrontend/App_Start/WebApiConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+
+namespace WebFrontend
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.SuppressDefaultHostAuthentication();
+            
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
